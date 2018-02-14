07101347a53d167088d645f04a6ce441039268e4
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-13 18:21:48 -0800

    Create visual-feedback.js

---
+++ visual-feedback.js
@@ -0,0 +1,95 @@
+var freqA = 261;
+var freqS = 293;
+var freqD = 329;
+var freqF = 349;
+
+var oscA, oscS, oscD, oscF;
+
+var playing = false;
+
+function setup() {
+  createCanvas (500, 500);
+  backgroundColor = color(235, 250, 255);
+  textAlign(CENTER);
+  
+  oscA = new p5.Oscillator();
+  oscA.setType('triangle');
+  oscA.freq(freqA);
+  oscA.amp(0);
+  oscA.start();
+  
+  
+  oscS = new p5.Oscillator();
+  oscS.setType('triangle');
+  oscS.freq(freqS);
+  oscS.amp(0);
+  oscS.start();
+  
+  oscD = new p5.Oscillator();
+  oscD.setType('triangle');
+  oscD.freq(freqD);
+  oscD.amp(0);
+  oscD.start();
+  
+  oscF = new p5.Oscillator();
+  oscF.setType('triangle');
+  oscF.freq(freqF);
+  oscF.amp(0);
+  oscF.start();
+}
+
+function draw() {
+  if (playing) {
+  } else {
+    background(235, 250, 255);
+  }
+}
+
+function keyPressed() {
+  text('NOW YOU SEE ME', width / 2, 40);
+  print("got key press for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+    noStroke()
+		fill (110, 220, 255)
+    ellipse (250, 250, freqA, freqA);
+  } else if (key == 'S') {
+    osc = oscS;
+    noStroke()
+		fill (0, 157, 255)
+    ellipse (250, 250, freqS, freqS);
+  } else if (key == 'D') {
+    osc = oscD;
+    noStroke()
+		fill (0, 123, 163)
+    ellipse (250, 250, freqD, freqD);
+  } else if (key == 'F') {
+    osc = oscF;
+    noStroke()
+		fill (0, 71, 94)
+    ellipse (250, 250, freqF, freqF);
+  }
+  if (osc) {
+    osc.amp(0.5, 0.1);
+    playing = true;
+  }
+}
+
+function keyReleased() {
+  print("got key release for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+  } else if (key == 'S') {
+    osc = oscS;
+  } else if (key == 'D') {
+    osc = oscD;
+  } else if (key == 'F') {
+    osc = oscF;
+  }
+  if (osc) {
+    osc.amp(0, 0.5);
+    playing = false;
+  }
+}
