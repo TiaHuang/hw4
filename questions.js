b5a4c58dbd1b9a5917d71d76ae886d1eaba0774b
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-14 17:42:09 -0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,14 @@
+What is an object?
+An object is a collection of properties that has data, behavior and more inside of it.
+
+How do you access a property of an object?
+To find the definition (variables) of the object in the code.
+
+What is the index of the first item in an array?
+0. 
+
+What syntax can you use to get the third item in an array?
+[0, 1, 2]
+
+How do you use a variable nad a loop to get every item in an array?
+Set the property as a variable and add a line in the code that tells it to print it's property every time the loop runs
